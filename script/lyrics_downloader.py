e906bd6047e0407c4dc9ae9fa3706f58363886f4
Paweł Mąsior <62357939+pmasior@users.noreply.github.com>
2024-06-16 00:32:39 +0200

    Add workaround for not found lyrics

--- script/lyrics_downloader.py
+++ script/lyrics_downloader.py
@@ -21,16 +21,22 @@ def _download_after_search(author, title):
     def _get_song_id_from_search(html_content):
         soup = BeautifulSoup(html_content, "lxml")
         selected_element = soup.select_one(".content .title")
-        song_href = selected_element["href"]
-        song_id_regex = re.search(r",([a-z0-9_,]+)\.", song_href)
-        return song_id_regex.group(1)
+        if selected_element is not None:
+            song_href = selected_element["href"]
+            song_id_regex = re.search(r",([a-z0-9_,]+)\.", song_href)
+            return song_id_regex.group(1)
+        else:
+            return None
 
     search_query = quote(f"{author} - {title}")
     html_content = webpage_downloader.download_webpage(
         f"https://www.tekstowo.pl/szukaj,{search_query}.html"
     )
     song_id = _get_song_id_from_search(html_content)
-    return _download_by_song_id(song_id)
+    if song_id is not None:
+        return _download_by_song_id(song_id)
+    else:
+        return r"<div class=song-text><div class=inner-text> </div></div>"
 
 
 def download_lyrics(author, title, song_id):
